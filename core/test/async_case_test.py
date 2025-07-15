814d977ae1b28c5862d7db40ce992e3ef1c4e100
dperfly <dongpengfei826153155@gmail.com>
2025-07-15 13:54:15 +0800

    deleted testcase

--- core/test/async_case_test.py
+++
@@ -1,7 +0,0 @@
-from core.case.runcase import AsyncRunCase
-
-from core.test.test_data import local_cache, TestFunc, raw_data
-
-
-def test_async_case_test():
-    AsyncRunCase(raw_data=raw_data, cache=local_cache.get_all_cache(), hook_func=TestFunc).run()
